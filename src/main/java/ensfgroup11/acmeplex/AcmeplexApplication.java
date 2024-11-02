83fb2ffd6eb1dd655d03f51c64aa40abd9df9fba
Steven <63431600+stevenhcau@users.noreply.github.com>
2024-11-01 18:35:10 -0600

    Respository setup
    Repository setup using spring initializr

---
+++ src/main/java/ensfgroup11/acmeplex/AcmeplexApplication.java
@@ -0,0 +1,13 @@
+package ensfgroup11.acmeplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeplexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ensfgroup11/acmeplex/AcmeplexApplicationTests.java
@@ -0,0 +1,13 @@
+package ensfgroup11.acmeplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
